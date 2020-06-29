9e25310ddbaea9cf4d6b621f404f64b42d3a3d32
AbSanzhar <mak.millan@mail.ru>
2020-06-30 01:08:17 +0600

    SoPN1

--- src/app/whole-page/opportunities/edit-employee-dialog/edit-employee-dialog.component.ts
+++ src/app/whole-page/opportunities/edit-employee-dialog/edit-employee-dialog.component.ts
@@ -9,11 +9,11 @@ import {DataControlService} from '../../../services/data-control.service';
   styleUrls: ['./edit-employee-dialog.component.css']
 })
 export class EditEmployeeDialogComponent implements OnInit {
-  // formOptions: FormGroup = new FormGroup({
-  //   option1: new FormControl(this.data.role[0] === 'professor' ? true : false),
-  //   option2: new FormControl(this.data.role[1] === 'department of science' ? true : false),
-  //   option3: new FormControl(this.data.role[2] === 'secretary of the council' ? true : false)
-  // });
+  formOptions: FormGroup = new FormGroup({
+    option1: new FormControl(this.data.roles[0].roleName === 'professor' ? true : false),
+    option2: new FormControl(this.data.roles[1].roleName === 'department of science' ? true : false),
+    option3: new FormControl(this.data.roles[2].roleName === 'secretary of the council' ? true : false)
+  });
 
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: any,
@@ -25,22 +25,22 @@ export class EditEmployeeDialogComponent implements OnInit {
   }
 
   saveChange() {
-    // let newForm = new FormGroup({
-    //   id: new FormControl(this.data.id),
-    //   firstName: new FormControl(this.data.firstName),
-    //   lastName: new FormControl(this.data.lastName),
-    //   middleName: new FormControl(this.data.middleName),
-    //   email: new FormControl(this.data.email),
-    //   role: new FormArray([
-    //     new FormControl(this.formOptions.getRawValue().option1 ? 'professor' : 'none'),
-    //     new FormControl(this.formOptions.getRawValue().option2 ? 'department of science' : 'none'),
-    //     new FormControl(this.formOptions.getRawValue().option3 ? 'secretary of the council' : 'none')
-    //   ])
-    // });
+    let newForm = new FormGroup({
+      id: new FormControl(this.data.id),
+      firstName: new FormControl(this.data.firstName),
+      lastName: new FormControl(this.data.lastName),
+      middleName: new FormControl(this.data.patronymic),
+      email: new FormControl(this.data.email),
+      role: new FormArray([
+        new FormControl(this.formOptions.getRawValue().option1 ? 'professor' : 'none'),
+        new FormControl(this.formOptions.getRawValue().option2 ? 'department of science' : 'none'),
+        new FormControl(this.formOptions.getRawValue().option3 ? 'secretary of the council' : 'none')
+      ])
+    });
 
-    // this.http.updateEmployess(newForm.getRawValue()).subscribe(response => {
-    //   this.dialog.close();
-    // });
+    this.http.updateEmployess(newForm.getRawValue()).subscribe(response => {
+      this.dialog.close();
+    });
   }
 
 }
