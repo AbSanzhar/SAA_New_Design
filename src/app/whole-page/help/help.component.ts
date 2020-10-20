6a8cc803b88fa206a4c223bac7a7e9e502d009f7
77773 <s.zhetpisbaeva>
2020-10-20 17:32:15 +0600

    Applying

--- src/app/whole-page/help/help.component.ts
+++ src/app/whole-page/help/help.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {FAQ} from '../../shared/model/FAQ';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-help',
@@ -8,16 +9,27 @@ import {FAQ} from '../../shared/model/FAQ';
 })
 export class HelpComponent implements OnInit {
 
-  faq: FAQ[] = [
-    {id: 1, question: 'Example question', answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', image: 'assets/images/faq/bell.svg'},
-    {id: 2, question: 'Example question', answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', image: 'assets/images/faq/photo.svg'},
-    {id: 3, question: 'Example question', answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', image: 'assets/images/faq/photo.svg'},
-    {id: 4, question: 'Example question', answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', image: 'assets/images/faq/bell.svg'},
-    {id: 5, question: 'Example question', answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', image: 'assets/images/faq/bell.svg'},
-    {id: 6, question: 'Example question', answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', image: 'assets/images/faq/bell.svg'},
-  ];
+  form: FormGroup;
 
-  constructor() { }
+  // faq: FAQ[] = [
+  //   {id: 1, question: 'Example question', answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', image: 'assets/images/faq/bell.svg'},
+  //   {id: 2, question: 'Example question', answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', image: 'assets/images/faq/photo.svg'},
+  //   {id: 3, question: 'Example question', answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', image: 'assets/images/faq/photo.svg'},
+  //   {id: 4, question: 'Example question', answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', image: 'assets/images/faq/bell.svg'},
+  //   {id: 5, question: 'Example question', answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', image: 'assets/images/faq/bell.svg'},
+  //   {id: 6, question: 'Example question', answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', image: 'assets/images/faq/bell.svg'},
+  // ];
+
+  constructor() {
+    this.form = new FormGroup({
+      organizationName: new FormControl('', Validators.required),
+      organizationType: new FormControl('', Validators.required),
+      bin: new FormControl('', Validators.required),
+      contactPerson: new FormControl('', Validators.required),
+      phone: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+    });
+  }
 
   ngOnInit(): void {
   }
